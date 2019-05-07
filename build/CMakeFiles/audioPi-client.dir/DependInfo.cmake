120c14c050136834d7dd4c705ce2f8c015c5cb78
szymon <szymon.janas.95@gmail.com>
2019-05-07 23:41:49 +0200

    release added

---
+++ build/CMakeFiles/audioPi-client.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/szymon/CppProjects/AudioPiStreamming/client/client-audio-processing.cpp" "/home/szymon/CppProjects/AudioPiStreamming/build/CMakeFiles/audioPi-client.dir/client/client-audio-processing.cpp.o"
+  "/home/szymon/CppProjects/AudioPiStreamming/client/client-main.cpp" "/home/szymon/CppProjects/AudioPiStreamming/build/CMakeFiles/audioPi-client.dir/client/client-main.cpp.o"
+  "/home/szymon/CppProjects/AudioPiStreamming/media/media.cpp" "/home/szymon/CppProjects/AudioPiStreamming/build/CMakeFiles/audioPi-client.dir/media/media.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/gstreamer-1.0"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "../media"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
