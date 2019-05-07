fdf88c5f96ebc6e2c3c6eead6fd3e530d0a0d628
szymon <szymon.janas.95@gmail.com>
2019-05-07 23:35:58 +0200

    added client class for recive and play music
    application for device who will be reciver music and playing it localy

--- client/client-audio-processing.cpp
+++ client/client-audio-processing.cpp
@@ -1,3 +1,47 @@
 #include"client-audio-processing.hpp"
 
+void Reciver_mp3::set_status(MediaStatus status)
+{
+    Media::set_status(status);
+}
 
+void Reciver_mp3::set_reciver_mp3(gchar* host, gint port)
+{
+    pipeline = gst_pipeline_new ("audio-streamer");
+    source   = gst_element_factory_make ("udpsrc",       "udp-source");
+   // queue    = gst_element_factory_make ("queue","queue");
+    depay    = gst_element_factory_make ("rtpmpadepay",  "depay");
+    parse    = gst_element_factory_make ("mpegaudioparse", "parse");
+    decoder  = gst_element_factory_make ("mpg123audiodec", "decoder");
+    convert  = gst_element_factory_make ("audioconvert", "convert");
+    resample = gst_element_factory_make ("audioresample", "resample");
+    sink     = gst_element_factory_make ("autoaudiosink", "sink");
+
+    if (!pipeline || !source //|| !queue
+            || !depay
+      || !parse || !convert || !decoder || !resample || !sink) {
+    g_printerr ("One element could not be created. Exiting.\n");
+    exit(1);
+    }
+
+    bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+    bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
+    gst_object_unref (bus);
+
+    gst_bin_add_many (GST_BIN (pipeline),
+    source, //queue,
+                      depay,parse, decoder, convert, resample, sink, NULL);
+    gst_element_link_many (source, //queue,
+                           depay, parse, decoder, convert, resample, sink, NULL);
+
+
+    GstCaps* caps;
+    caps = gst_caps_new_simple("application/x-rtp", NULL);
+    //gboolean link = gst_element_link_filtered(source, depay, caps);
+    //if (!link) g_warning("Failed to link element with caps! ");
+    g_object_set(G_OBJECT(source), "caps", caps, NULL);
+    gst_caps_unref(caps);
+
+    g_object_set(G_OBJECT(source), "address", host, "port", port, NULL);
+    g_print("HERE");
+}
---
+++ client/client-audio-processing.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <gst/gst.h>
+
+#include "../media/media.hpp"
+
+class Reciver_mp3 : private Media
+{
+    GstElement *queue, *depay, *decoder, *convert, *resample;
+public:
+    void set_reciver_mp3(gchar* host, gint port);
+    void set_status(MediaStatus status);
+};
