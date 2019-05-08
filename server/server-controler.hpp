0668940c21d1b7d37c1dadaaeb1bb9802bda5428
szymon <szymon.janas.95@gmail.com>
2019-05-08 12:05:47 +0200

    fix error

--- server/server-controler.hpp
+++ server/server-controler.hpp
@@ -1,12 +1,7 @@
 #pragma once
 
-#include <boost/asio.hpp>
-
-using boost::asio::ip::tcp
 
 class Controler
 {
-    public:
-    void set_controlling_device();
-    void start_reciving_udp(std::string format);
+
 };
